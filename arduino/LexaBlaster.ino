ea7faf0400b1dddf7ebac93f6766aa5e58da5211
Josh KIttle <josh.kittle@gmail.com>
2021-07-16 20:42:29 -0400

    Initial Commit
    Initial Commit

---
+++ arduino/LexaBlaster.ino
@@ -0,0 +1,280 @@
+#include "defines.h"
+#include <IRLibSendBase.h>    //We need the base code
+#include <IRLib_HashRaw.h>    //Only use raw sender
+
+IRsendRaw mySender;
+#define LED_PIN 3
+#define DEBUG_LOCAL   2
+#define WIFI_GOOD 7
+#define WIFI_BAD 6
+#define DEBUG_PIN 13
+// Pin 3 is the IR LED Pin 
+
+
+#define RAW_DATA_LEN 36
+uint16_t rawData[RAW_DATA_LEN]={
+  1014, 1502, 494, 506, 494, 1498, 498, 502, 
+  494, 506, 494, 1502, 494, 1498, 498, 502, 
+  494, 506, 494, 1498, 498, 502, 498, 1498, 
+  494, 1502, 498, 502, 494, 506, 494, 1498, 
+  498, 1498, 494, 1000};
+
+
+#include <WebSockets2_Generic.h>
+#include <ArduinoJson.h>          // https://arduinojson.org/ or install via Arduino library manager
+
+using namespace websockets2_generic;
+
+WebsocketsClient client;
+
+#define HEARTBEAT_INTERVAL      300000 // 5 Minutes 
+
+bool isConnected                = false;
+uint64_t heartbeatTimestamp     = 0;
+uint64_t now                    = 0;
+
+//To increase no of devices as needed
+
+
+
+void SendIR()
+{
+   mySender.send(rawData,RAW_DATA_LEN,38);//Pass the buffer,length, optionally frequency
+   
+}
+
+void turnOn(String deviceId)
+{
+  if (deviceId == SINRIC_Device_ID_1) // Device ID of 1st device
+  {
+#if (DEBUG_LOCAL > 1)
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+#endif
+    SendIR();
+    digitalWrite(DEBUG_PIN, HIGH);
+  }
+  else
+  {
+#if (DEBUG_LOCAL > 1)
+    Serial.print("Turn on for unknown device id: ");
+    Serial.println(deviceId);
+#endif
+  }
+}
+
+//To increase no of devices as needed
+
+void turnOff(String deviceId)
+{
+
+  if (deviceId == SINRIC_Device_ID_1) // Device ID of 1st device
+    
+  {
+  
+
+#if (DEBUG_LOCAL > 1)
+    Serial.print("Turn off Device ID: ");
+    Serial.println(deviceId);
+#endif
+    SendIR();
+    digitalWrite(DEBUG_PIN, LOW);
+  }
+  else
+  {
+#if (DEBUG_LOCAL > 1)
+    Serial.print("Turn off for unknown device id: ");
+    Serial.println(deviceId);
+#endif
+  }
+}
+
+void onEventsCallback(WebsocketsEvent event, String data)
+{
+  if (event == WebsocketsEvent::ConnectionOpened)
+  {
+    if (!isConnected)
+      isConnected = true;
+
+    Serial.println("Connnection Opened");
+  }
+  else if (event == WebsocketsEvent::ConnectionClosed)
+  {
+    if (isConnected)
+      isConnected = false;
+
+    Serial.println("Connnection Closed");
+  }
+  else if (event == WebsocketsEvent::GotPing)
+  {
+    if (!isConnected)
+      isConnected = true;
+
+    Serial.println("Got a Ping!");
+  }
+  else if (event == WebsocketsEvent::GotPong)
+  {
+    if (!isConnected)
+      isConnected = true;
+
+    Serial.println("Got a Pong!");
+  }
+}
+
+void onMessagesCallback(WebsocketsMessage message)
+{
+  String SINRIC_message = message.data();
+
+  Serial.print("Got Message: ");
+  Serial.println(SINRIC_message /*message.data()*/);
+
+  // Example payloads
+
+  // For Switch or Light device types
+  // {"deviceId": xxxx, "action": "setPowerState", value: "ON"} // https://developer.amazon.com/docs/device-apis/alexa-powercontroller.html
+
+  // For Light device type
+  // Look at the light example in github
+
+#if (ARDUINOJSON_VERSION_MAJOR >= 6)
+  DynamicJsonDocument json(1024);
+  //auto deserializeError = deserializeJson(json, (char*)message.data());
+  auto deserializeError = deserializeJson(json, SINRIC_message);
+
+  if ( deserializeError )
+  {
+    Serial.println("JSON parseObject() failed");
+    return;
+  }
+  //serializeJson(json, Serial);
+#else
+  DynamicJsonBuffer jsonBuffer;
+  // Parse JSON string
+  JsonObject& json = jsonBuffer.parseObject(SINRIC_message);
+  // Test if parsing succeeds.
+  if (!json.success())
+  {
+    Serial.println("JSON parseObject() failed");
+    return;
+  }
+#endif
+
+  String deviceId = json ["deviceId"];
+  String action = json ["action"];
+
+  if (action == "setPowerState")
+  {
+    // Switch or Light
+    String value = json ["value"];
+    if (value == "ON")
+    {
+      turnOn(deviceId);
+    }
+    else
+    {
+      turnOff(deviceId);
+    }
+  }
+}
+
+void setup()
+{
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(WIFI_GOOD, OUTPUT);
+  pinMode(WIFI_BAD, OUTPUT);
+  pinMode(DEBUG_PIN, OUTPUT);
+  
+  digitalWrite(LED_PIN, LOW);
+  digitalWrite(WIFI_GOOD, LOW);
+  digitalWrite(WIFI_BAD, LOW);
+  digitalWrite(DEBUG_PIN, LOW);
+
+  Serial.begin(115200);
+  delay(5000);
+  //while (!Serial);
+
+  Serial.println("\nStarting SAMD-Client_SINRIC with WiFiNINA on " + String(BOARD_NAME));
+  Serial.println(WEBSOCKETS2_GENERIC_VERSION);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE)
+  {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    return;
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+
+  // Connect to wifi
+  WiFi.begin(ssid, password);
+
+  // Wait some time to connect to wifi
+  for (int i = 0; i < 20 && WiFi.status() != WL_CONNECTED; i++)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    
+    digitalWrite(WIFI_GOOD, HIGH);
+    digitalWrite(WIFI_BAD, LOW);
+    Serial.print("Connected to Wifi, IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("Connecting to WebSockets Server @");
+    Serial.println(websockets_server_host);
+  }
+  else
+  {
+    Serial.println("\nNo WiFi");
+    digitalWrite(WIFI_BAD, HIGH);
+    digitalWrite(WIFI_GOOD, LOW);
+    return;
+  }
+
+  // run callback when events are occuring
+  client.onMessage(onMessagesCallback);
+
+  // try to connect to Websockets server
+  client.addHeader("apikey", SINRIC_API_KEY);
+  client.setAuthorization("apikey", SINRIC_API_KEY);
+
+  bool connected = client.connect(websockets_server_host, websockets_server_port, "/");
+
+  if (connected)
+  {
+    Serial.println("Connected!");
+  }
+  else
+  {
+    Serial.println("Not Connected!");
+  }
+}
+
+void loop()
+{
+  // let the websockets client check for incoming messages
+  if (client.available())
+  {
+    client.poll();
+
+    now = millis();
+
+    // Send heartbeat in order to avoid disconnections during ISP resetting IPs over night. Thanks @MacSass
+    if ((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL)
+    {
+      heartbeatTimestamp = now;
+      client.send("H");
+    }
+  }
+}
